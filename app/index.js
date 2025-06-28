499032f4b8af65040a4c09254988db5f75362858
Muhsin95952 <muhsinshah95952@gmail.com>
2025-06-28 16:35:59 +0500

    Trigger deploy

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello I am Muhsin Shah. I hope you will be fine and this I hope you will be fine and this is Node.js CI/CD Pipeline!');
+  res.send('Hello I am Muhsin Shah. I hope you will be fine and this  is Node.js CI/CD Pipeline!');
 });
 
 app.listen(port, () => {
