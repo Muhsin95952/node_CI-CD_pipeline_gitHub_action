4f6516b30d1a982b1d433e0b5d508e3f989e053d
Muhsin Shah <111670341+Muhsin95952@users.noreply.github.com>
2025-07-14 12:54:08 +0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send(' Muhsin Shah.!');
+  res.send('I am  Muhsin Shah. And who are you!');
 });
 
 app.listen(port, () => {
